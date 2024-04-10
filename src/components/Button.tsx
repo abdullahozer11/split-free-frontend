ce138f68d2f2d97de375335ad87e027711486bd0
Abdullah OZER <abdullahozer11@hotmail.com>
2024-04-10 08:21:52 +0200

    Custom component can be styled

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,18 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, StyleProp, ViewStyle } from 'react-native';
 import Colors from '../constants/Colors';
 import React, { forwardRef } from 'react';
 
 type ButtonProps = {
   text: string;
+  style?: StyleProp<ViewStyle>;
+  textStyle?: StyleProp<ViewStyle>;
 } & React.ComponentPropsWithoutRef<typeof Pressable>;
 
 const Button = forwardRef<View | null, ButtonProps>(
-  ({ text, ...pressableProps }, ref) => {
+  ({ text, style, textStyle, ...pressableProps }, ref) => {
     return (
-      <Pressable ref={ref} {...pressableProps} style={styles.container}>
-        <Text style={styles.text}>{text}</Text>
+      <Pressable ref={ref} {...pressableProps} style={[styles.container, style]}>
+        <Text style={[styles.text, textStyle]}>{text}</Text>
       </Pressable>
     );
   }
