f31fe130ea0d0c852091aab12cae6c4c0b87296a
Abdullah OZER <abdullahozer11@hotmail.com>
2024-04-18 09:22:19 +0200

    Nested modal is a better solution for now

--- src/modals/CreateGroup.tsx
+++ src/modals/CreateGroup.tsx
@@ -6,33 +6,39 @@ import {
   TextInput,
   Pressable,
   ScrollView,
-  Modal, TouchableOpacity,
+  Modal,
+  TouchableOpacity,
 } from "react-native";
-import {useAuth} from "@/src/providers/AuthProvider";
-import {useNavigation} from "expo-router";
-import MemberRow from "@/src/components/MemberRow";
-import {Feather, Ionicons} from "@expo/vector-icons";
+import {Feather} from '@expo/vector-icons';
+import Button from '@/src/components/Button';
 
 const CreateGroupModal = ({isVisible, onClose, onDraw}) => {
   const [title, setTitle] = useState("");
-  const {profile} = useAuth();
-  const initialMember = {
-    name: profile?.name || "John Doe",
-    profile: profile,
-  }
-  const [members, setMembers] = useState([initialMember]);
-  const navigation = useNavigation();
+  const [members, setMembers] = useState([]);
+  const [showParticipantsModal, setShowParticipantsModal] = useState(false);
+  const [memberName, setMemberName] = useState();
 
-  function addNewMember() {
-    console.log("adding new member");
-  }
+  const addNewMember = () => {
+    console.log("Adding new member");
+    // Logic to add a new member
+  };
 
-  function removeMember(index) {
-    console.log("removing member: ", index);
-  }
+  const removeMember = (index) => {
+    console.log("Removing member:", index);
+    // Logic to remove a member
+  };
 
   const saveData = async () => {
-    console.log("saving data");
+    console.log("Saving data");
+    // Logic to save group data
+  };
+
+  const openParticipantsModal = () => {
+    setShowParticipantsModal(true);
+  };
+
+  const closeParticipantsModal = () => {
+    setShowParticipantsModal(false);
   };
 
   return (
@@ -48,7 +54,7 @@ const CreateGroupModal = ({isVisible, onClose, onDraw}) => {
             <TouchableOpacity onPress={onClose}>
               <Feather name="x" size={24} color="black"/>
             </TouchableOpacity>
-            <Pressable textstyle={styles.done} onPress={saveData}>
+            <Pressable onPress={saveData}>
               <Text style={styles.doneText}>Done</Text>
             </Pressable>
           </View>
@@ -58,19 +64,53 @@ const CreateGroupModal = ({isVisible, onClose, onDraw}) => {
             value={title}
             onChangeText={(text) => setTitle(text)}
           />
-          <TouchableOpacity style={styles.addParticipants} onPress={onDraw}>
+          <TouchableOpacity style={styles.addParticipants} onPress={openParticipantsModal}>
             <Text style={styles.parTitle}>Add Participants</Text>
           </TouchableOpacity>
           <ScrollView>
             {members.map((member, index) => (
-              <MemberRow key={index} member={member}/>
+              <Text>{member}</Text>
             ))}
           </ScrollView>
         </View>
       </View>
+      {showParticipantsModal && (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={showParticipantsModal}
+          onRequestClose={closeParticipantsModal}
+        >
+          <View style={styles.modalContainer2}>
+            <View style={styles.container2}>
+              <View style={styles.pModalInnerHeader}>
+                <Text style={styles.pModalTitle}>Add Participants</Text>
+                <TextInput
+                  value={memberName}
+                  onChangeText={setMemberName}
+                  placeholder="Member Name"
+                  style={styles.pModalInput}
+                />
+              </View>
+              <View style={styles.pModalFooter}>
+                <Button style={styles.pModalBtn} textStyle={styles.pModalBtnText} text="Cancel"
+                        onPress={closeParticipantsModal}/>
+                <Button style={styles.pModalBtn} textStyle={styles.pModalBtnText} text="Done"
+                        onPress={closeParticipantsModal}/>
+              </View>
+            </View>
+            <View style={styles.container3}>
+              <Text>TODO</Text>
+            </View>
+          </View>
+        </Modal>
+      )}
     </Modal>
   );
 };
+
+export default CreateGroupModal;
+
 
 const styles = StyleSheet.create({
   modalContainer: {
@@ -120,6 +160,65 @@ const styles = StyleSheet.create({
     borderStyle: "dashed",
     paddingVertical: 10,
     marginBottom: 5,
+  },
+  modalContainer2: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  container2: {
+    position: "absolute",
+    top: "2%",
+    right: 0,
+    width: "70%",
+    height: "96%",
+    borderTopStartRadius: 20,
+    borderBottomStartRadius: 20,
+    padding: 20,
+    backgroundColor: "gainsboro",
+    justifyContent: "space-between",
+  },
+  pModalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 50,
+  },
+  pModalInput: {
+    width: 200,
+    height: 40,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10,
+  },
+  pModalFooter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    gap: 40,
+  },
+  pModalFooterButtonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+  },
+  pModalBtn: {
+    backgroundColor: 'transparent',
+    padding: 10,
+    marginBottom: 16,
+  },
+  pModalBtnText: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  container3: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: 100,
+    height: 100,
   },
 });
 
