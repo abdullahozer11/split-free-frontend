9b74a695fd20ce5278f4b8e08a224fb3548a8bb7
Abdullah OZER <abdullahozer11@hotmail.com>
2024-05-04 13:20:45 +0200

    Remove redundant functions

--- src/api/members/index.ts
+++ src/api/members/index.ts
@@ -1,5 +1,5 @@
 import {supabase} from "@/src/lib/supabase";
-import {useQuery, useMutation, useQueryClient} from "@tanstack/react-query";
+import {useQuery} from "@tanstack/react-query";
 
 
 export const useMemberList = (groupId: number) => {
@@ -14,42 +14,6 @@ export const useMemberList = (groupId: number) => {
         // console.log(error.message);
         throw new Error(error.message);
       }
-      return data;
-    },
-  });
-};
-
-export const usePayerList = (expense_id: number) => {
-  return useQuery({
-    queryKey: ['expense_payers'],
-    queryFn: async () => {
-      const {data, error} = await supabase
-        .from('expense_payers')
-        .select('id, member(name, profile(avatar_url))')
-        .eq('expense', expense_id);
-      if (error) {
-        console.log(error.message);
-        throw new Error(error.message);
-      }
-      // console.log('expense payers are', data);
-      return data;
-    },
-  });
-};
-
-export const useParticipantList = (expense_id: number) => {
-  return useQuery({
-    queryKey: ['expense_participants'],
-    queryFn: async () => {
-      const {data, error} = await supabase
-        .from('expense_participants')
-        .select('id, member(name, profile(avatar_url))')
-        .eq('expense', expense_id);
-      if (error) {
-        // console.log(error.message);
-        throw new Error(error.message);
-      }
-      // console.log('expense participants are', data);
       return data;
     },
   });
