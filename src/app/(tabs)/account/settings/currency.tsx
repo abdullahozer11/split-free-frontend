841788668a0c8642dae82f8cb9f7adf93a9e01cf
Abdullah OZER <abdullahozer11@hotmail.com>
2024-05-15 20:03:40 +0200

    Create currency selector

--- src/app/(tabs)/account/settings/currency.tsx
+++ src/app/(tabs)/account/settings/currency.tsx
@@ -1,11 +1,26 @@
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import {Text} from 'react-native-paper';
 import React from "react";
 import {SafeAreaView} from "react-native-safe-area-context";
+import {CurrencySelector} from "@/src/components/CurrencySelector";
+import {Feather} from "@expo/vector-icons";
+import {useNavigation} from "expo-router";
 
 const Currency = () => {
+  const navigation = useNavigation();
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Currency</Text>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => {
+          navigation.goBack();
+        }}>
+          <Feather name={"arrow-left"} size={36}/>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.body}>
+        <Text variant={'headlineLarge'}>Select Currency</Text>
+        <CurrencySelector/>
+      </View>
     </SafeAreaView>
   );
 };
@@ -13,5 +28,18 @@ const Currency = () => {
 export default Currency;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1
+  },
+  header: {
+    height: 60,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  body: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
 });
