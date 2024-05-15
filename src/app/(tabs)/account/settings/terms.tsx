75c635faae495fbada90b359ffcd09487675599c
Abdullah OZER <abdullahozer11@hotmail.com>
2024-05-15 20:21:04 +0200

    Terms added.

--- src/app/(tabs)/account/settings/terms.tsx
+++ src/app/(tabs)/account/settings/terms.tsx
@@ -1,12 +1,26 @@
-import {StyleSheet, View, Text} from 'react-native';
+import {ScrollView, StyleSheet, TouchableOpacity} from 'react-native';
+import {Card, Text} from 'react-native-paper';
 import React from "react";
 import {SafeAreaView} from "react-native-safe-area-context";
-
+import {termsText} from "@/TermsOfUse";
+import {useNavigation} from "expo-router";
+import {Feather} from "@expo/vector-icons";
 
 const Terms = () => {
+  const navigation = useNavigation();
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Terms</Text>
+        <TouchableOpacity onPress={() => {navigation.goBack()}}>
+          <Feather name={"arrow-left"} size={36}/>
+        </TouchableOpacity>
+      <ScrollView>
+        <Card style={styles.card}>
+          <Card.Content>
+            <Text style={{alignSelf: "center"}} variant={'headlineLarge'}>TERM OF USE</Text>
+            <Text style={styles.text}>{termsText}</Text>
+          </Card.Content>
+        </Card>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -14,5 +28,24 @@ const Terms = () => {
 export default Terms;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#F6F6F6FF",
+  },
+  header: {
+    marginTop: 10,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  card: {
+    marginVertical: 8,
+    backgroundColor: "white",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
 });
