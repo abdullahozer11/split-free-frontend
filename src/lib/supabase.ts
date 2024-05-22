d6bb7cdc09e463d8f9d86a203d717fe374ec0c80
Abdullah OZER <abdullahozer11@hotmail.com>
2024-05-22 10:53:44 +0200

    More on eas config for builds

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -29,7 +29,7 @@ const ExpoSecureStoreAdapter = {
 
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || "";
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
