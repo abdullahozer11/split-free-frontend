73b40a8e21ba5852cd074a1bf57429d37e24e7b6
Abdullah OZER <abdullahozer11@hotmail.com>
2024-05-25 21:27:25 +0200

    Add google component

--- src/components/google.tsx
+++ src/components/google.tsx
@@ -15,6 +15,7 @@ const GoogleSignIn = () => {
     try {
       await GoogleSignin.hasPlayServices();
       const userInfo = await GoogleSignin.signIn();
+      console.log("userInfo is", userInfo);
       if (userInfo.idToken) {
         const { data, error } = await supabase.auth.signInWithIdToken({
           provider: 'google',
