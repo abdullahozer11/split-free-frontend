c28a20283d6a38be7f585ce51dbe83119516dc52
Abdullah OZER <abdullahozer11@hotmail.com>
2024-06-21 20:34:03 +0200

    global tabs header is removed.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -59,6 +59,7 @@ function RootLayoutNav() {
             <Stack>
               <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
               <Stack.Screen name="(auth)" options={{headerShown: false}}/>
+              <Stack.Screen name="(global)" options={{headerShown: false}}/>
             </Stack>
             {/*</NotificationProvider>*/}
           </PaperProvider>
