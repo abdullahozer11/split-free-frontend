2b13f281c23cd000db250b946f4626c631c80b30
Abdullah OZER <abdullahozer11@hotmail.com>
2024-06-23 20:42:21 +0200

    Order expense categories alphabetically

--- src/utils/expense_categories.ts
+++ src/utils/expense_categories.ts
@@ -1,35 +1,5 @@
 export const exp_cats = [
   {
-    'name': 'Shopping',
-    'icon': 'shopping-cart',
-    'icon_color': '#000000',
-    'bg_color': '#FFC107',
-  },
-  {
-    'name': 'Groceries',
-    'icon': 'food-bank',
-    'icon_color': '#FFFFFF',
-    'bg_color': '#FF5722',
-  },
-  {
-    'name': 'Dining Out',
-    'icon': 'restaurant',
-    'icon_color': '#FFFFFF',
-    'bg_color': '#673AB7',
-  },
-  {
-    'name': 'Home & Utilities',
-    'icon': 'home',
-    'icon_color': '#000000',
-    'bg_color': '#4CAF50',
-  },
-  {
-    'name': 'Travel',
-    'icon': 'airplanemode-active',
-    'icon_color': '#FFFFFF',
-    'bg_color': '#F44336',
-  },
-  {
     'name': 'Car & Transportation',
     'icon': 'directions-car',
     'icon_color': '#FFFFFF',
@@ -42,34 +12,10 @@ export const exp_cats = [
     'bg_color': '#CDDC39',
   },
   {
-    'name': 'Electronics & Gadgets',
-    'icon': 'computer',
+    'name': 'Dining Out',
+    'icon': 'restaurant',
     'icon_color': '#FFFFFF',
-    'bg_color': '#FFEB3B',
-  },
-  {
-    'name': 'Hobbies & Leisure',
-    'icon': 'menu-book',
-    'icon_color': '#000000',
-    'bg_color': '#00BCD4',
-  },
-  {
-    'name': 'Entertainment',
-    'icon': 'sports-esports',
-    'icon_color': '#FFFFFF',
-    'bg_color': '#009688',
-  },
-  {
-    'name': 'Health & Fitness',
-    'icon': 'monitor-heart',
-    'icon_color': '#FFFFFF',
-    'bg_color': '#795548',
-  },
-  {
-    'name': 'Personal Care',
-    'icon': 'child-care',
-    'icon_color': '#000000',
-    'bg_color': '#FF9800',
+    'bg_color': '#673AB7',
   },
   {
     'name': 'Education',
@@ -78,10 +24,64 @@ export const exp_cats = [
     'bg_color': '#607D8B',
   },
   {
+    'name': 'Electronics & Gadgets',
+    'icon': 'computer',
+    'icon_color': '#FFFFFF',
+    'bg_color': '#FFEB3B',
+  },
+  {
+    'name': 'Entertainment',
+    'icon': 'sports-esports',
+    'icon_color': '#FFFFFF',
+    'bg_color': '#009688',
+  },
+  {
     'name': 'Gifts & Donations',
     'icon': 'card-giftcard',
     'icon_color': '#000000',
     'bg_color': '#03A9F4',
+  },
+  {
+    'name': 'Groceries',
+    'icon': 'food-bank',
+    'icon_color': '#FFFFFF',
+    'bg_color': '#FF5722',
+  },
+  {
+    'name': 'Health & Fitness',
+    'icon': 'monitor-heart',
+    'icon_color': '#FFFFFF',
+    'bg_color': '#795548',
+  },
+  {
+    'name': 'Hobbies & Leisure',
+    'icon': 'menu-book',
+    'icon_color': '#000000',
+    'bg_color': '#00BCD4',
+  },
+  {
+    'name': 'Home & Utilities',
+    'icon': 'home',
+    'icon_color': '#000000',
+    'bg_color': '#4CAF50',
+  },
+  {
+    'name': 'Personal Care',
+    'icon': 'child-care',
+    'icon_color': '#000000',
+    'bg_color': '#FF9800',
+  },
+  {
+    'name': 'Shopping',
+    'icon': 'shopping-cart',
+    'icon_color': '#000000',
+    'bg_color': '#FFC107',
+  },
+  {
+    'name': 'Travel',
+    'icon': 'airplanemode-active',
+    'icon_color': '#FFFFFF',
+    'bg_color': '#F44336',
   },
   {
     'name': 'Other',
