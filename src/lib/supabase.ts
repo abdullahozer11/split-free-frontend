2828511b8b7e2aeb777e070c630c6be8138f5e37
abdullahozer11 <abdullahozer11@hotmail.com>
2024-12-26 21:24:38 +0100

    Revert "Fix envvar not matching"
    This reverts commit 6dfb1d6976253e838887abc65915bdf451a42213.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -28,10 +28,7 @@ const ExpoSecureStoreAdapter = {
 };
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || "";
-
-console.log('supabaseUrl', supabaseUrl);
-console.log('supabaseAnonKey', supabaseAnonKey);
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
