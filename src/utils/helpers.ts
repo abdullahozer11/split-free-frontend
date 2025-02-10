f212aae92636cd7f2327463cbe096169350ad43c
Abdullah OZER <abdullahozer11@hotmail.com>
2025-02-10 20:00:03 +0100

    Some changes

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,3 @@
-import * as FileSystem from "expo-file-system";
-import { randomUUID } from "expo-crypto";
-import { supabase } from "@/src/lib/supabase";
-import { decode } from "base64-arraybuffer";
-
 export const groupElementsByDay = (elements) => {
   const groupedElements = {};
   elements.forEach((activity) => {
@@ -20,57 +15,10 @@ export const groupElementsByDay = (elements) => {
   return groupedElements;
 };
 
-export function Hidden(props: { children }) {
-  return null;
-}
-
 // Generate a formatted date
 export function getFormattedDate(date) {
   const options = { day: "numeric", month: "short", year: "numeric" };
   return date?.toLocaleDateString("en-US", options);
-}
-
-export const uploadImage = async (image) => {
-  if (!image?.startsWith("file://")) {
-    return;
-  }
-
-  const base64 = await FileSystem.readAsStringAsync(image, {
-    encoding: "base64",
-  });
-  const filePath = `${randomUUID()}.png`;
-  const contentType = "image/png";
-
-  const { data, error } = await supabase.storage
-    .from("expense-proof-images")
-    .upload(filePath, decode(base64), { contentType });
-
-  console.log("Error while uploading", error);
-
-  if (data) {
-    return data.path;
-  }
-};
-
-export function findArrayDiff(array1, array2) {
-  const added = [];
-  const subtracted = [];
-
-  // Find added numbers
-  array2.forEach((num) => {
-    if (!array1.includes(num)) {
-      added.push(num);
-    }
-  });
-
-  // Find subtracted numbers
-  array1.forEach((num) => {
-    if (!array2.includes(num)) {
-      subtracted.push(num);
-    }
-  });
-
-  return { added, subtracted };
 }
 
 export function formatDate(dateObj) {
