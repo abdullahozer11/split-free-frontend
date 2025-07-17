76b460fe7521ce19f1cc7d6b7317dc976d88fd27
abdullahozer11 <abdullahozer11@hotmail.com>
2025-07-17 16:40:13 +0200

    usecolorscheme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,10 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
-import { useColorScheme } from "@/src/components/useColorScheme";
 import AuthProvider from "@/src/providers/AuthProvider";
 import QueryProvider from "@/src/providers/QueryProvider";
 import { PaperProvider } from "react-native-paper";
@@ -52,10 +47,8 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <AuthProvider>
         <QueryProvider>
           <PaperProvider>
