d95249e2d21317f13487484b6badd7dcfc696197
abdullahozer11 <abdullahozer11@hotmail.com>
2025-07-18 19:42:39 +0200

    stuff

--- android/app/src/main/java/xyz/splitfree/MainActivity.kt
+++ android/app/src/main/java/xyz/splitfree/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.splitfree
+package xyz.splitfree
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/xyz/splitfree/MainApplication.kt
+++ android/app/src/main/java/xyz/splitfree/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.splitfree
+package xyz.splitfree
 
 import android.app.Application
 import android.content.res.Configuration
