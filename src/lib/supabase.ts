a337ef11acd8181cc800dc9a5723ea9a8394370f
Abdullah OZER <abdullahozer11@hotmail.com>
2025-08-03 12:32:46 +0200

    Display supabaseUrl at startup

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -30,6 +30,8 @@ const ExpoSecureStoreAdapter = {
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
 
+console.log("supabaseUrl is", supabaseUrl);
+
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: ExpoSecureStoreAdapter as any,
