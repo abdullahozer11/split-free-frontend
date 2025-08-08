1f6100d22ab3f6313ac7c38eae857c2544be79f2
abdullahozer11 <abdullahozer11@hotmail.com>
2025-08-08 10:39:22 +0200

    todo REMOVE dont push

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -27,8 +27,8 @@ const ExpoSecureStoreAdapter = {
   },
 };
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "http://192.168.1.72:54321";
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
 
 console.log("supabaseUrl is", supabaseUrl);
 
